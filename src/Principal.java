689aaca347d01031b0f43deccff12d34ffbc45a8
UserPL135Pc07 <UserPL135Pc07@SLFRPL135PC07.ensino.atec.local>
2016-10-26 11:33:33 +0100

    Calculo do balanço

--- src/Principal.java
+++ src/Principal.java
@@ -8,6 +8,26 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ola turma!");
+		
+		
+		int quotas 		= 300,
+			recBaile 	= 200,
+			recBar 	 	= 250;
+		
+		int totalReceitas = quotas + recBaile + recBar;
+		
+		System.out.println("Receitas: " + totalReceitas  + "€");
+		
+		int luz			= 80,
+			agua		= 60,
+			telefone	= 70;
+		
+		int totalDespesas =  luz + agua + telefone;
+		
+		System.out.println("Despesas: " + totalDespesas + "€");
+		
+		System.out.println("\nBalanço: " + (totalReceitas - totalDespesas)  + "€");
+		
 	}
 
 }
